8551a2d4e187dabe96e3a9579cc14010ef5dcc26
kana-sucre <guillaume-pillon@live.fr>
2014-06-08 08:19:36 +0200

    Connexion cas

--- src/utc/assos/payutcapp/MainActivity.java
+++ src/utc/assos/payutcapp/MainActivity.java
@@ -1,12 +1,19 @@
 package utc.assos.payutcapp;
 
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+
+import utc.assos.payutcapp.communication.CasConnexion;
+import utc.assos.payutcapp.communication.RequestTicket;
+import utc.assos.payutcapp.login.LoginActivity;
+import utc.assos.payutcapp.prefs.PrefsManager;
 
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
@@ -18,6 +25,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
 
@@ -39,7 +47,18 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		String login = PrefsManager.getLogin(this);
+		String password = PrefsManager.getLogin(this);
+		
+		if(login == null || password == null){
+			Intent intent = new Intent(this, LoginActivity.class);
+			startActivity(intent);
+			this.finish();
+		}else{
+			connexion(login, password);
+		}
+		
 		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -212,6 +231,69 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 			}
 			return rootView;
 		}
+	}
+	
+	public void connexion(String login, String password){
+		if(login != null && password != null){
+			String tbt = ConnexionCas(login, password);
+			if (tbt!= null && tbt.startsWith("TGT")){
+				String ticket = getTicket(tbt, getResources().getString(R.string.service));
+				if (ticket!=null && ticket.startsWith("ST")){
+					//TODO registration
+					Intent intent = new Intent(this, MainActivity.class);
+					startActivity(intent);
+					this.finish();
+					
+				}else{
+					failAuthentification();
+				}
+				
+			}else{
+				failAuthentification();
+			}
+		}
+	}
+	
+	public void failAuthentification(){
+		PrefsManager.setLogin(this, null);
+		PrefsManager.setPassword(this, null);
+		Toast.makeText(this, "Erreur d'authentification", Toast.LENGTH_SHORT).show();
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+		this.finish();
+	}
+	
+	public void registerInfo(String login, String password){
+		PrefsManager.setLogin(this, login);
+		PrefsManager.setPassword(this, password);
+	}
+	
+	public String ConnexionCas(String user, String pass){
+		try {
+			CasConnexion casConnexion = new CasConnexion(this);
+			return casConnexion.execute(user, pass).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public String getTicket(String tbt, String service){
+		try {
+			RequestTicket rt = new RequestTicket(this);
+			return rt.execute(tbt, service).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
--- src/utc/assos/payutcapp/login/LoginActivity.java
+++ src/utc/assos/payutcapp/login/LoginActivity.java
@@ -4,10 +4,17 @@ import java.util.concurrent.ExecutionException;
 
 import utc.assos.payutcapp.communication.CasConnexion;
 import utc.assos.payutcapp.communication.RequestTicket;
+import utc.assos.payutcapp.MainActivity;
 import utc.assos.payutcapp.R;
 import utc.assos.payutcapp.prefs.PrefsManager;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -19,12 +26,31 @@ public class LoginActivity extends Activity {
 		String login = PrefsManager.getLogin(this);
 		String password = PrefsManager.getLogin(this);
 		
+		final EditText edit_login = (EditText) findViewById(R.id.edit_login);
+		final EditText edit_pass = (EditText) findViewById(R.id.edit_password);
+		
+		Button connect = (Button) findViewById(R.id.connect);
+		
+		connect.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				connexion(edit_login.getText().toString(), edit_pass.getText().toString());
+			}
+		});
+
+	}
+	
+	public void connexion(String login, String password){
 		if(login != null && password != null){
 			String tbt = ConnexionCas(login, password);
 			if (tbt!= null && tbt.startsWith("TGT")){
 				String ticket = getTicket(tbt, getResources().getString(R.string.service));
 				if (ticket!=null && ticket.startsWith("ST")){
 					//TODO registration
+					Intent intent = new Intent(this, MainActivity.class);
+					startActivity(intent);
+					this.finish();
 					
 				}else{
 					failAuthentification();
@@ -34,12 +60,12 @@ public class LoginActivity extends Activity {
 				failAuthentification();
 			}
 		}
-	
 	}
 	
 	public void failAuthentification(){
 		PrefsManager.setLogin(this, null);
 		PrefsManager.setPassword(this, null);
+		Toast.makeText(this, "Erreur d'authentification", Toast.LENGTH_SHORT).show();
 	}
 	
 	public void registerInfo(String login, String password){
